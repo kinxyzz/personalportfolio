dc87bafed0313caf70f05cc96697546c9fa9f01e
kinxyzz <133165609+kinxyzz@users.noreply.github.com>
2024-03-13 03:07:50 +0700

    Update WorkDetail.tsx

--- src/components/templates/WorkDetail.tsx
+++ src/components/templates/WorkDetail.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 
 export default function WorkDetail() {
-  let { id } = useParams();
+
 
   return (
     <div className="flex flex-col items-center ">
